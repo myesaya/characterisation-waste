694453e1d6595bdebcfd043844b249b7973a881d
myesaya <yesayamabvuto@gmail.com>
2024-01-11 13:40:50 +0200

    import data

---
+++ docs/index.R
@@ -0,0 +1,13 @@
+library (tidyverse)
+library(here)
+
+
+# -------------------------------------------------------------------------
+
+# Load data
+setwd("~/gitrepos/characterisation-waste/data/raw")
+
+  characterisation <- read_csv("wcs-waste-composition.csv")
+
+
+
